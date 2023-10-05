3ddbcd968c9ba49e367c772b840c3329971e0778
Annkkitaaa <ankitasingh15.102@gmail.com>
2023-10-06 01:49:30 +0530

    binary search

---
+++ Day-11/lowerbound.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void lowerBound(int arr[],int n,int x){
+    int low=0;
+    int high=n-1;
+    int ans=n;
+    while(low<high){
+        int mid=(low+high)/2;
+        if(arr[mid]>=x){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+
+    cout<<ans;
+
+}
+
+int main(){
+    int x=2;
+    int Nums[] = {1,2,2,3};
+    int n=sizeof(Nums)/sizeof(Nums[0]);
+    lowerBound(Nums,n,x);
+    return 0;
+}
